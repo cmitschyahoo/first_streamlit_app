c76d6853ce3635584e16ad29d79d309a93835fec
cmitschyahoo <122922990+cmitschyahoo@users.noreply.github.com>
2023-04-03 09:44:57 -0700

    Update streamlit_app.py
    img

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import streamlit
 streamlit.title('New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Green Eggs and Ham')
+streamlit.text('🥣 Green Eggs and Ham')
+streamlit.text('🥑🍞 Green Avocado and Toast')
