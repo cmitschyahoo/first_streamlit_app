0d9c9cbf6c19cf54f349d119e54abbe139a24836
cmitschyahoo <122922990+cmitschyahoo@users.noreply.github.com>
2023-04-06 09:25:44 -0700

    Update streamlit_app.py
    kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # who likes to look at json?
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # How about some normalized json??
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json()) # who likes to look at json?
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # How about some normalized json into a table?
 streamlit.dataframe(fruityvice_normalized)
 
