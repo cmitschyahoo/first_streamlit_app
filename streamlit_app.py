5614dadd1183baf2a0e19aef8be4ed169fdaaf48
cmitschyahoo <122922990+cmitschyahoo@users.noreply.github.com>
2023-04-10 08:57:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.title('New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Green Eggs and Ham')
 streamlit.text('🥑🍞 Green Avocado and Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothies 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
